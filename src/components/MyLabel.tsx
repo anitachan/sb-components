6983e992f0257b5a6484be5030a6937c89310cef
Ana Lucia Chavez <achavez@avidxchange.com>
2022-03-01 16:07:47 -0500

    adding background color

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -21,6 +21,10 @@ export interface Props {
    * Personalized font color 
    */
    fontColor?: string;
+  /**
+   * Personalized font color 
+   */
+   backgroundColor?: string;
 }
 
 export const MyLabel = ({
@@ -28,10 +32,11 @@ export const MyLabel = ({
   size = "normal",
   color = "primary",
   allCaps = false,
-  fontColor
+  fontColor,
+  backgroundColor = 'transparent'
 }: Props) => {
   return (
-    <span className={`label ${size} text-${color}`} style={{ color: fontColor }} >
+    <span className={`label ${size} text-${color}`} style={{ color: fontColor, backgroundColor }} >
       { allCaps ? label.toLocaleUpperCase() :  label}
     </span>
   );
